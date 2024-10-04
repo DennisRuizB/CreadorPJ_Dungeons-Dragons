ee20e1531c17e4571ad1538e1a638a67f869ee39
MohaLourd <128591509+MohaLourd@users.noreply.github.com>
2024-10-04 11:58:18 +0200

    Add files via upload

---
+++ CreadorPJ_Dungeons-Dragons-main version victor/CreadorPJ_Dungeons-Dragons-main version victor/CreadorPJ_Dungeons-Dragons-main/PersonajesDungeonsAndDragons/src/VariablesGlobales.java
@@ -0,0 +1,6 @@
+public class VariablesGlobales
+{
+          public static int Elfo_puede_crearse = 0;
+            public static int Enano_puede_crearse = 0;
+           public static int Mediano_puede_crearse = 0;
+       }
