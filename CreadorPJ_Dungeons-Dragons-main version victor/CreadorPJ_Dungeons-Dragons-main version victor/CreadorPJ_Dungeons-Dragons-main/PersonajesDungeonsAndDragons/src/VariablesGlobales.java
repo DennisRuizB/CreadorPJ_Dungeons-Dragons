25836885020019b76e4ecfdbccff62bb0fdf2b0e
MARCOGAR2004 <127140905+MARCOGAR2004@users.noreply.github.com>
2024-10-06 21:31:09 +0200

    Delete CreadorPJ_Dungeons-Dragons-main version victor directory

--- CreadorPJ_Dungeons-Dragons-main version victor/CreadorPJ_Dungeons-Dragons-main version victor/CreadorPJ_Dungeons-Dragons-main/PersonajesDungeonsAndDragons/src/VariablesGlobales.java
+++
@@ -1,6 +0,0 @@
-public class VariablesGlobales
-{
-          public static int Elfo_puede_crearse = 0;
-            public static int Enano_puede_crearse = 0;
-           public static int Mediano_puede_crearse = 0;
-       }
